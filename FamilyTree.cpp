97a962f51c65208cf316f8ba543eabe8b4186bdc
Dean <carmideanGmail.com>
2020-04-16 11:32:28 +0300

    dean1

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -2,12 +2,51 @@
 using namespace family;
 
 Tree& Tree::addFather(string child, string father){
+    Tree::addFather(child, father, this->root);
+    return *this;
+}
+
+Tree& Tree::addFather(string child, string father, Person* start){
+    if (start != nullptr){
+        if (start->name == child){
+            if (start->father != nullptr){
+                throw RuleException(child + "- already has a father");
+            }
+            Person* p = new Person(father);
+            start->father = p;
+            return *this;
+        }
+        this->addFather(child, father, start->father);
+        this->addFather(child, father, start->mother);
+    }
     return *this;
 } 
+
+
  Tree& Tree::addMother(string child, string mother){
-     return *this;
+    Tree::addMother(child, mother, this->root); 
+    return *this;
  }
-string Tree::relation(string family_memeber_name){
+Tree& Tree::addMother(string child, string mother, Person* start){
+    if (start != nullptr){
+        if(start->name == child){
+            if (start->mother != nullptr){
+                throw RuleException(child + "- already has a mother");
+            }
+            Person* p = new Person(mother);
+            start->mother = p;
+            return *this;
+        }
+        this->addMother(child, mother, start->father);
+        this->addMother(child, mother, start->mother);
+    }
+    return *this;
+}
+
+
+
+
+string Tree::relation(string relate){
     return " ";
 }
 string Tree::find(string family_relation ){
