969fe0ce0c758bf8aa8bd697c44e2a0679dc14eb
Dean <carmideanGmail.com>
2020-04-16 14:44:45 +0300

    dean tests

--- Test.cpp
+++ Test.cpp
@@ -4,7 +4,13 @@
 using namespace std;
 using namespace family;
 
-
+/**
+ *                                        yosef
+ *                  avi                                            rivka
+ *        mira             moshe                        israel              rina
+ * 
+ * 
+ */
 
 Tree getTree(){
     Tree t("yosef");
@@ -17,7 +23,7 @@ Tree getTree(){
     return t;
 }
 
- 
+
 
 TEST_CASE("Test for find function"){
     
@@ -117,9 +123,122 @@ TEST_CASE("Test for relation function")
 
 
 TEST_CASE("Test for addFather function"){
-    
+    Tree tree = getTree();
+    // Basics tests
+    CHECK_NOTHROW(tree.addFather("moshe", "natan"));
+    CHECK_NOTHROW(tree.addFather("natan", "natan"));
+
+    //Trying to overwrite an existing father
+    CHECK_THROWS_AS(tree.addFather("yosef", "bar"), RuleException);
+    CHECK_THROWS_AS(tree.addFather("avi", "shlomi"), RuleException);
+
+    //Trying non existing family members
+    CHECK_THROWS_AS(tree.addFather("abcd", "yosef"), RuleException);
+    CHECK_THROWS_AS(tree.addFather("1234", "elad"), RuleException);
+
+    //Adding an empty name
+    CHECK_NOTHROW(tree.addFather("mira", ""));
+    CHECK(tree.relation("great-grandfather") == string(""));
+    tree.remove("mira");
+    tree.addMother("avi", "mira");
+
+    //A space as a name for a person
+    CHECK_NOTHROW(tree.addFather("mira", " "));
+    CHECK(tree.relation("great-grandfather") == string(" "));
+    tree.remove(" ");
+
+    //Checking addFather worked, creating a new tree of only fathers
+    Tree t("jack");
+    t.addFather("jack", "shalom")
+        .addFather("shalom", "tzadok")
+        .addFather("tzadok", "moshe");
+    CHECK(tree.relation("shalom") == string("father"));
+    CHECK(tree.find("father") == string("shalom"));
+    CHECK(tree.relation("tzadok") == string("grandfather"));
+    CHECK(tree.find("grandfather") == string("tzadok"));
+    //Removing tzadok and his father moshe from tree
+    t.remove("tzadok");
+    CHECK_NOTHROW(tree.addFather("shalom", "iftah"));
+
+    //Adding persons wih indetical name, but with different  
+    //position in the family tree
+    /**               INIT              FIRST CHECK         SECOND CHECK
+     *                jack                jack                  jack
+     *          shalom          ->     shalom       ->       shalom
+     *      iftah                   iftah                 iftah
+     *                            iftah                 iftah
+     *                                                iftah
+     *                         
+     */
+    t.addFather("iftah", "iftah")
+        .addFather("iftah", "iftah");
+    CHECK(t.find("great-grandfather") == string("iftah"));
+    CHECK(t.find("great-great-grandfather") == string("iftah"));
+
+    delete &t;
+    delete &tree;
 }
 TEST_CASE("Test for addMother function"){
-    
+    Tree t("yoni");
+    CHECK_NOTHROW(t.addMother("yoni", "haya"));
+    CHECK_NOTHROW(t.addMother("haya", "rahel"));
+    CHECK_NOTHROW(t.addMother("rahel", "gila"));
+
+    CHECK(t.find("mother") == string("haya"));
+    CHECK(t.find("grandmother") == string("rahel"));
+    CHECK(t.find("great-grandmother") == string("gila"));
+    //Mother already exists
+    CHECK_THROWS(t.addMother("yoni", "haya")); 
+    CHECK_THROWS(t.addMother("haya", "rina")); 
+    //Child doesn't exists
+    CHECK_THROWS(t.addMother("daniel", "abcd"));
+
+    //Adding an empty name
+    CHECK_NOTHROW(t.addMother("gila", ""));
+    CHECK(t.relation("great-great-grandmother") == string(""));
+
+    //A space as a name for a person
+    CHECK_NOTHROW(t.addMother("", " "));
+
+    delete &t;
+}
+TEST_CASE("Test for remove function"){
+    Tree t = getTree();
+    //Removing root
+    CHECK_THROWS(t.remove("yosef"));
+    //Removing a person hat doesn't exists
+    CHECK_THROWS(t.relation("aaaaa"));
+    //Removing a sub-family
+    CHECK_NOTHROW(t.remove("rivka"));
+    CHECK_NOTHROW(t.addMother("yosef","rahel"));
+    CHECK(t.relation("israel") == string("unrelated"));
+    CHECK(t.relation("rina") == string("unrelated"));
+    CHECK(t.relation("rivka") == string("unrelated"));
+
+    //Removing already removed persons
+    CHECK_THROWS(t.remove("israel"));
+    CHECK_THROWS(t.remove("rina"));
+    CHECK_THROWS(t.remove("rivka"));
+
+    //Removing a different sub-family
+    CHECK_NOTHROW(t.remove("avi"));
+    CHECK(t.relation("avi") == string("unrelated"));
+    CHECK(t.relation("mira") == string("unrelated"));
+    CHECK(t.relation("moshe") == string("unrelated"));
+    CHECK_NOTHROW(t.addFather("yosef", "eli"));
+
+    //Another sub-family
+    t.addFather("eli", "avraham")
+     .addMother("eli", "sara")
+     .addFather("avraham", "shulman")
+     .addMother("avraham", "shula");
+    CHECK_NOTHROW(t.remove("eli"));
+    CHECK(t.relation("eli") == string("unrelated"));
+    CHECK(t.relation("avraham") == string("unrelated"));
+    CHECK(t.relation("sara") == string("unrelated"));
+    CHECK(t.relation("shulman") == string("unrelated"));
+    CHECK(t.relation("shula") == string("unrelated"));
+
+    delete &t;
 }
 
